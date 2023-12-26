6378d841679342f3629d34bb0a722a4397330519
Muhamad Ridwan <73826477+g4ridwan@users.noreply.github.com>
2023-12-26 16:34:34 +0700

    Update openstack_collect.sh

--- script-metrics/openstack_collect.sh
+++ script-metrics/openstack_collect.sh
@@ -7,6 +7,8 @@ sleep 3
 openstack server list --all-projects --host Server01 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /root/script-metrics/Server01
 openstack server list --all-projects --host Server02 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /root/script-metrics/Server02
 openstack server list --all-projects --host Server03 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /root/script-metrics/Server03
+openstack server list --all-projects --host Server04 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /root/script-metrics/Server04
+openstack server list --all-projects --host Server05 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /root/script-metrics/Server05
 ## Using "sed" to delete space,symbols,character,dll
 ## Exit
 exit
