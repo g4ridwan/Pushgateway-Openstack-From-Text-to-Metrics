9e13953442fad62f86a6899211f1473cf2655a21
Muhamad Ridwan <73826477+g4ridwan@users.noreply.github.com>
2023-12-26 17:52:11 +0700

    Update openstack_collect.sh
    I'm using openstack command to retrieve list of instance, then write output into text file

--- script-metrics/openstack_collect.sh
+++ script-metrics/openstack_collect.sh
@@ -4,11 +4,11 @@ source /root/script-metrics/Your_StackRC
 ## Wait for Openstack StackRC to load
 sleep 3
 ## Write Output to File
-openstack server list --all-projects --host Server01 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /root/script-metrics/Server01
-openstack server list --all-projects --host Server02 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /root/script-metrics/Server02
-openstack server list --all-projects --host Server03 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /root/script-metrics/Server03
-openstack server list --all-projects --host Server04 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /root/script-metrics/Server04
-openstack server list --all-projects --host Server05 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /root/script-metrics/Server05
+openstack server list --all-projects --host Server01 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /home/ops-metrics/script-metrics/Server01
+openstack server list --all-projects --host Server02 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /home/ops-metrics/script-metrics/Server02
+openstack server list --all-projects --host Server03 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /home/ops-metrics/script-metrics/Server03
+openstack server list --all-projects --host Server04 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /home/ops-metrics/script-metrics/Server04
+openstack server list --all-projects --host Server05 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /home/ops-metrics/script-metrics/Server05
 ## Using "sed" to delete space,symbols,character,dll
 ## Exit
 exit
