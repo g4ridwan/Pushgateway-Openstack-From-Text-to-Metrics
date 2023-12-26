ccd16e4522b9842ebc8891c5dc34c2e0bdf3b50f
Muhamad Ridwan <73826477+g4ridwan@users.noreply.github.com>
2023-12-26 18:10:26 +0700

    Update Services-Check.sh

--- script-metrics/Services-Check.sh
+++ script-metrics/Services-Check.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 status="$(systemctl is-active Services_Name])"
 if [ "$status" = "active" ]; then
-    echo "Services_Name 1" | curl --data-binary @- http://localhost:9091/metrics/job/Services/Instance/compute-01
+    echo "Services_Name 1" | curl --data-binary @- http://localhost:9091/metrics/job/Services/Instance/Server01
 else
-    echo "Service_Name 0" | curl --data-binary @- http://localhost:9091/metrics/job/Services/Instance/compute-01
+    echo "Service_Name 0" | curl --data-binary @- http://localhost:9091/metrics/job/Services/Instance/Server01
+    exit 1
+fi
+status="$(systemctl is-active Services_Name])"
+if [ "$status" = "active" ]; then
+    echo "Services_Name 1" | curl --data-binary @- http://localhost:9091/metrics/job/Services/Instance/Server02
+else
+    echo "Service_Name 0" | curl --data-binary @- http://localhost:9091/metrics/job/Services/Instance/Server02
+    exit 1
+fi
+status="$(systemctl is-active Services_Name])"
+if [ "$status" = "active" ]; then
+    echo "Services_Name 1" | curl --data-binary @- http://localhost:9091/metrics/job/Services/Instance/Server03
+else
+    echo "Service_Name 0" | curl --data-binary @- http://localhost:9091/metrics/job/Services/Instance/Server03
     exit 1
 fi
