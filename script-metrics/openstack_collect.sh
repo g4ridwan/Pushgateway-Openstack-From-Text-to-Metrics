fe6bc170242884ae34c22af6ad12a6489732b056
Muhamad Ridwan <73826477+g4ridwan@users.noreply.github.com>
2023-12-26 16:27:31 +0700

    Add files via upload

---
+++ script-metrics/openstack_collect.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+## Load "stackrc" to Session
+source /root/script-metrics/Your_StackRC
+## Wait for Openstack StackRC to load
+sleep 3
+## Write Output to File
+openstack server list --all-projects --host Server01 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /root/script-metrics/Server01
+openstack server list --all-projects --host Server02 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /root/script-metrics/Server02
+openstack server list --all-projects --host Server03 -c Name -f csv | awk -F '","' 'NR>1{gsub(/"/, ""); print $1}' | sed 's/\[//;s/\]//' > /root/script-metrics/Server03
+## Using "sed" to delete space,symbols,character,dll
+## Exit
+exit
